b28c5e93fb394d29deb16cb561da818bedfc6ecd
Yipeng Sun <syp@umd.edu>
2019-06-03 12:26:46 -0400

    Install sublime.

--- arch/scripts/install-dev.sh
+++ arch/scripts/install-dev.sh
@@ -10,3 +10,8 @@ chsh -s /usr/bin/zsh vagrant
 # Install docker
 /usr/bin/pacman -S --noconfirm docker
 /usr/bin/usermod -aG docker vagrant
+
+# Install sublime
+/usr/bin/curl -O https://download.sublimetext.com/sublimehq-pub.gpg && /usr/bin/pacman-key --add sublimehq-pub.gpg && /usr/bin/pacman-key --lsign-key 8A8F901A && rm sublimehq-pub.gpg
+echo -e "\n[sublime-text]\nServer = https://download.sublimetext.com/arch/stable/x86_64" | /usr/bin/tee -a /etc/pacman.conf
+/usr/bin/pacman -Syu --noconfirm sublime-text
