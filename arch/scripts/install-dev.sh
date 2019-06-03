05acfe03e19d0cfaeba44abda05e67a63a1c53d2
Yipeng Sun <syp@umd.edu>
2019-06-03 12:08:18 -0400

    Configure zsh.

--- arch/scripts/install-dev.sh
+++ arch/scripts/install-dev.sh
@@ -3,3 +3,9 @@
 # Install & configure shell
 /usr/bin/pacman -S --noconfirm zsh
 chsh -s /usr/bin/zsh vagrant
+
+# Install git & friends
+/usr/bin/pacman -S --noconfirm git tig git-annex
+
+# Install docker
+/usr/bin/pacman -S --noconfirm docker
