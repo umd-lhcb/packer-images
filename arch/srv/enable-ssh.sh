4545f8b7a872eb5e8471c25c8036f20119845961
Yipeng Sun <syp@umd.edu>
2019-06-03 00:18:47 -0400

    Make sure the user name is consistent.

--- arch/srv/enable-ssh.sh
+++ arch/srv/enable-ssh.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-PASSWORD=$(/usr/bin/openssl passwd -crypt 'vagrant')
+PASSWORD=$(/usr/bin/openssl passwd -crypt 'developer')
 
 echo "==> Enabling SSH"
 # Vagrant-specific configuration
-/usr/bin/useradd --password ${PASSWORD} --comment 'Vagrant User' --create-home --user-group vagrant
-echo 'Defaults env_keep += "SSH_AUTH_SOCK"' > /etc/sudoers.d/10_vagrant
-echo 'vagrant ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers.d/10_vagrant
-/usr/bin/chmod 0440 /etc/sudoers.d/10_vagrant
+/usr/bin/useradd --password ${PASSWORD} --comment 'Developer' --create-home --user-group developer
+echo 'Defaults env_keep += "SSH_AUTH_SOCK"' > /etc/sudoers.d/10_developer
+echo 'developer ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers.d/10_developer
+/usr/bin/chmod 0440 /etc/sudoers.d/10_developer
 /usr/bin/systemctl start sshd.service
