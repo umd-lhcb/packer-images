f03cf56974307308fce13b2288fb4b50db4b2b2e
Yipeng Sun <syp@umd.edu>
2019-06-03 03:46:49 -0400

    Don't write any 0s.

--- README.md
+++ README.md
@@ -2,5 +2,12 @@
 Packer images for reproducible develop environments.
 
 
+## Build
+Install `packer` and `virtualbox` in your system. Then go to `arch` folder:
+```
+packer build arch.json
+```
+
+
 ## Acknowledgment
 Based on [packer-arch](https://github.com/elasticdog/packer-arch).
--- arch/scripts/cleanup.sh
+++ arch/scripts/cleanup.sh
@@ -4,7 +4,7 @@
 /usr/bin/yes | /usr/bin/pacman -Scc
 
 # Write zeros to improve virtual disk compaction.
-zerofile=$(/usr/bin/mktemp /zerofile.XXXXX)
-/usr/bin/dd if=/dev/zero of="$zerofile" bs=1M
-/usr/bin/rm -f "$zerofile"
-/usr/bin/sync
+#zerofile=$(/usr/bin/mktemp /zerofile.XXXXX)
+#/usr/bin/dd if=/dev/zero of="$zerofile" bs=1M
+#/usr/bin/rm -f "$zerofile"
+#/usr/bin/sync
