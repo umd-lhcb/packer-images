08f136ddb731d10e4da1d04fa5ea90ada3fa62ea
Yipeng Sun <syp@umd.edu>
2019-06-03 11:40:19 -0400

    Install a browser.

--- arch/scripts/install-de.sh
+++ arch/scripts/install-de.sh
@@ -7,3 +7,6 @@
 /usr/bin/pacman -S --noconfirm xfce4
 echo -e 'exec startxfce4' > /home/vagrant/.xinitrc
 chown vagrant:vagrant /home/vagrant/.xinitrc
+
+# Install a web browser
+/usr/bin/pacman -S --noconfirm firefox
