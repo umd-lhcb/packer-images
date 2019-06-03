b78893c652ea6bdeeeeddf5b272320ca9e7e4411
Yipeng Sun <syp@umd.edu>
2019-06-03 12:03:35 -0400

    Run all VirtualBox helpers at X startup.

--- arch/scripts/install-de.sh
+++ arch/scripts/install-de.sh
@@ -5,7 +5,8 @@
 
 # Install Xfce
 /usr/bin/pacman -S --noconfirm xfce4
-echo -e 'exec startxfce4' > /home/vagrant/.xinitrc
+echo -e 'VBoxClient-all' > /home/vagrant/.xinitrc
+echo -e 'exec startxfce4' >> /home/vagrant/.xinitrc
 chown vagrant:vagrant /home/vagrant/.xinitrc
 
 # Install a web browser
